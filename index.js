e3988339907f000842cc723057ad8e72d58142a8
george kinyanjui1 <george.kinyajui@student.moringaschool.com>
2022-07-01 13:46:50 +0300

    Done.

--- index.js
+++ index.js
@@ -1,78 +1,8 @@
-/* Your Code Here */
-
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
-
-const allWagesFor = function() {
-    const eligibleDates = this.timeInEvents.map(function(e) {
-        return e.date
-    })
-
-    const payable = eligibleDates.reduce(function(memo, d) {
-            return memo + wagesEarnedOnDate.call(this, d)
-        }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
-
-    return payable
-}
-const createEmployeeRecord = function(recArray) {
-    return {
-        firstName: recArray[0],
-        familyName: recArray[1],
-        title: recArray[2],
-        payPerHour: recArray[3],
-        timeInEvents: [],
-        timeOutEvents: []
-    };
-};
-
-const createEmployeeRecords = function(recsArray) {
-    return recsArray.map(rec => createEmployeeRecord(rec));
-};
-
-const createTimeInEvent = function(datestamp) {
-    let [date, hour] = datestamp.split(' ');
-    this.timeInEvents.push({
-        type: 'TimeIn',
-        hour: parseInt(hour),
-        date: date
-    });
-    return this
-};
-
-const createTimeOutEvent = function(datestamp) {
-    let [date, hour] = datestamp.split(' ');
-    this.timeOutEvents.push({
-        type: 'TimeOut',
-        hour: parseInt(hour),
-        date: date
-    });
-    return this
-};
-
-const hoursWorkedOnDate = function(date) {
-    const inEvent = this.timeInEvents.find(inEvent => inEvent.date === date);
-    const outEvent = this.timeOutEvents.find(outEvent => outEvent.date === date);
-    return ((outEvent.hour - inEvent.hour) / 100)
-};
-
-const wagesEarnedOnDate = function(date) {
-    return hoursWorkedOnDate.apply(this, [date]) * this.payPerHour
-};
-const findEmployeeByFirstName = function(srcArray, targetName) {
-    for (let i = 0; i < srcArray.length; i++) {
-        if (srcArray[i].firstName === targetName) {
-            return srcArray[i]
-        }
-    }
-};
-
-const calculatePayroll = function(recsArray) {
-    let payroll = []
-    recsArray.forEach(element => payroll.push(allWagesFor.call(element)))
-    return payroll.reduce((a, b) => { return a + b })
-}
+// // Your code goes here
+// document.addEventListener('DOMContentLoaded', () => {
+//     let text = document.querySelector("#text")
+//     text.textContent = "This is really cool!";
+// })
+document.addEventListener("DOMContentLoader", function() {
+    console.log("DOMcontent loaded")
+});
